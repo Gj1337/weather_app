4fbfdd5e159788481975521e1dcbbbfdca2447da
GjDude <cool.nice847@gmail.com>
2024-04-17 04:27:12 +0300

    GitHub actions setup

--- scripts/codogen.sh
+++ scripts/codogen.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 flutter pub get
 dart run easy_localization:generate --source-dir ./assets/translations -o locale_keys.g.dart -f keys
 dart run build_runner build --delete-conflicting-outputs
